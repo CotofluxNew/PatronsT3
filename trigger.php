30efd52b00494a7ffb423882191e9b517ac7e9c0
CotofluxNew <cotoflux@gmail.com>
2023-02-24 21:38:05 +0100

    implementado el getCounter()

--- trigger.php
+++ trigger.php
@@ -2,6 +2,7 @@
 class Tigger {
 
     private static $instancia = null;
+    private static $counter = 0;
 
     public function __construct() {
             echo "Building character..." . PHP_EOL;
@@ -15,7 +16,12 @@ class Tigger {
         if(!isset(self::$instancia )){
             self::$instancia  = new Tigger();
         }
+        self::$counter++;
         return self::$instancia ;
+    }
+
+    public static function getCounter(){
+        return self::$counter++;
     }
 
 }
@@ -23,6 +29,8 @@ class Tigger {
 $instancia1 = Tigger::getInstance();
 $instancia2 = Tigger::getInstance();
 $instancia3 = Tigger::getInstance();
+
+echo "<br/>El número de veces que el contador se ha ejecutado es ".Tigger::getCounter()."<br/>" ;
 
 $instancia1::roar();
 $instancia2::roar();
